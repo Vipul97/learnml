4078403af848226d61dabdea2a24e5e3a91b4a96
Vipul Gharde <vipul.gharde@gmail.com>
2019-11-23 03:46:50 +0530

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='learnml',
-    version='0.0.1',
+    version='0.0.2',
     author='Vipul Gharde',
     description='LearnML is a Python module for Machine Learning and Deep Learning.',
     url='https://github.com/Vipul97/learnml',
