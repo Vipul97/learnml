8fbe5abb88a8a450337707c66cacf49bd9419da0
Vipul Gharde <vipul.gharde@gmail.com>
2019-11-25 00:04:30 +0530

    Updated Linear Regression Implementation

--- learnml/linear_model/_base.py
+++ learnml/linear_model/_base.py
@@ -8,7 +8,7 @@ class LinearRegression:
     def fit(self, X, y):
         self.X = X
         self.y = y
-        self.w = np.linalg.pinv(np.c_[np.ones((self.X.shape[0], 1)), self.X]).dot(self.y)
+        self.intercept_, *self.coef_ = np.linalg.pinv(np.c_[np.ones((self.X.shape[0], 1)), self.X]).dot(self.y)
 
     def predict(self, X):
-        return np.dot(np.c_[np.ones((X.shape[0], 1)), X], self.w)
+        return np.dot(np.c_[np.ones((X.shape[0], 1)), X], np.concatenate([[self.intercept_], self.coef_]))
