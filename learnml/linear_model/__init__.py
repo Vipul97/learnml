94a3f6253603d599490c83f3990509c1366665c9
Vipul Gharde <vipul.gharde@gmail.com>
2019-11-26 00:54:53 +0530

    Implemented Logistic Regression

--- learnml/linear_model/__init__.py
+++ learnml/linear_model/__init__.py
@@ -1,13 +1,13 @@
 from ._base import LinearRegression
 
-from .stochastic_gradient import SGDRegressor
+from .stochastic_gradient import SGDClassifier, SGDRegressor
 from .ridge import Ridge
-from ._logistic import LogisticRegression
 from ._perceptron import Perceptron
 
 __all__ = ['LinearRegression',
            'LogisticRegression',
            'Perceptron',
            'Ridge',
+           'SGDClassifier',
            'SGDRegressor'
 ]
--- learnml/linear_model/stochastic_gradient.py
+++ learnml/linear_model/stochastic_gradient.py
@@ -1,12 +1,51 @@
+from learnml.utils import sigmoid
 import numpy as np
+
+
+class SGDClassifier:
+    def __init__(self, max_iter=1000, eta0=0.0):
+        self.n_iter_ = max_iter
+        self.__eta0 = eta0
+
+    def fit(self, X, y):
+        def gradient_descent():
+            costs = []
+            w = [self.intercept_, *self.coef_]
+
+            for iter in range(self.n_iter_):
+                p = sigmoid(np.dot(X, w))
+
+                cost = -1 / m * (np.sum(y * np.log(p) + (1 - y) * np.log(1 - p)))
+                w -= self.__eta0 * 1 / m * np.dot(X.T, p - y)
+
+                costs.append(cost)
+
+            self.intercept_, *self.coef_ = w
+
+            return costs
+
+        X = np.c_[np.ones((X.shape[0], 1)), X]
+        m = X.shape[0]
+        self.intercept_, *self.coef_ = np.zeros(X.shape[1])
+        self.costs = gradient_descent()
+
+    def predict(self, X):
+        X = np.c_[np.ones((X.shape[0], 1)), X]
+        y_pred = np.zeros(X.shape[0])
+        w = [self.intercept_, *self.coef_]
+
+        p = sigmoid(np.dot(X, w))
+        y_pred[p > 0.5] = 1
+
+        return y_pred
 
 
 class SGDRegressor:
     def __init__(self, penalty='l2', alpha=0.0001, max_iter=1000, eta0=0.01):
-        self.penalty = penalty
-        self.alpha = alpha
-        self.max_iter = max_iter
-        self.eta0 = eta0
+        self.__penalty = penalty
+        self.__alpha = alpha
+        self.n_iter_ = max_iter
+        self.__eta0 = eta0
 
     def fit(self, X, y):
         def gradient_descent():
@@ -19,17 +58,17 @@ class SGDRegressor:
             costs, w_path = [], []
             w = [self.intercept_, *self.coef_]
 
-            for iter in range(self.max_iter):
-                for i in range(self.m):
-                    cost = (np.dot(self.X[i], w) - y[i]) ** 2
-                    w -= self.eta0 * 2 * np.dot(self.X[i].T, np.dot(self.X[i], w) - y[i])
+            for iter in range(self.n_iter_):
+                for i in range(m):
+                    cost = (np.dot(X[i], w) - y[i]) ** 2
+                    w -= self.__eta0 * 2 * np.dot(X[i].T, np.dot(X[i], w) - y[i])
 
-                    if self.penalty == 'l2':
-                        cost += self.alpha * np.dot(w[1:].T, w[1:])
-                        w[1:] -= 2 * self.alpha * w[1:]
-                    elif self.penalty == 'l1':
-                        cost += self.alpha * np.sum(np.abs(w[1:]))
-                        w[1:] -= 2 * self.alpha * sign(w[1:])
+                    if self.__penalty == 'l2':
+                        cost += self.__alpha * np.dot(w[1:].T, w[1:])
+                        w[1:] -= 2 * self.__alpha * w[1:]
+                    elif self.__penalty == 'l1':
+                        cost += self.__alpha * np.sum(np.abs(w[1:]))
+                        w[1:] -= 2 * self.__alpha * sign(w[1:])
 
                     costs.append(cost)
                     w_path.append(w.copy())
@@ -38,10 +77,9 @@ class SGDRegressor:
 
             return costs, np.array(w_path)
 
-        self.X = np.c_[np.ones((X.shape[0], 1)), X]
-        self.y = y
-        self.m = self.X.shape[0]
-        self.intercept_, *self.coef_ = np.random.randn(self.X.shape[1])
+        X = np.c_[np.ones((X.shape[0], 1)), X]
+        m = X.shape[0]
+        self.intercept_, *self.coef_ = np.random.randn(X.shape[1])
         self.costs, self.w_path = gradient_descent()
 
     def predict(self, X):
