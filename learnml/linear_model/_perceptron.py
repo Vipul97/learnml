d59d96a79a59764cb6d2db67bcf4e239c277cb7f
Vipul Gharde <vipul.gharde@gmail.com>
2019-12-03 21:01:02 +0530

    Updated Perceptron Implementation

--- learnml/linear_model/_perceptron.py
+++ learnml/linear_model/_perceptron.py
@@ -3,22 +3,22 @@ import numpy as np
 
 class Perceptron:
     def __init__(self, max_iter=1000, eta0=1.0):
-        self.max_iter = max_iter
-        self.eta0 = eta0
+        self.__max_iter = max_iter
+        self.__eta0 = eta0
 
     def fit(self, X, y):
         n_samples = X.shape[0]
         X = np.c_[np.ones((n_samples, 1)), X]
         n_features = X.shape[1]
         self.intercept_, *self.coef_ = np.zeros(n_features)
-        self.n_iter = self.max_iter
+        self.n_iter = self.__max_iter
         self.t_ = self.n_iter * n_samples
         w = [self.intercept_, *self.coef_]
 
         for j in range(self.n_iter):
             for i in range(n_samples):
                 y_hat = np.heaviside(np.dot(X[i], w), 0)
-                w += self.eta0 * (y[i] - y_hat) * X[i]
+                w += self.__eta0 * (y[i] - y_hat) * X[i]
 
         self.intercept_, *self.coef_ = w
 
