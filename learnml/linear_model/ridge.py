78697b20878ee5d247a2d9f0fc4bc206f3230007
Vipul Gharde <vipul.gharde@gmail.com>
2019-12-03 20:48:41 +0530

    Updated Ridge Regression Implementation

--- learnml/linear_model/ridge.py
+++ learnml/linear_model/ridge.py
@@ -4,17 +4,18 @@ import numpy as np
 class Ridge:
     def __init__(self, alpha=1.0):
         self.alpha = alpha
+        self.n_iter_ = None
 
     def fit(self, X, y):
-        self.X = np.c_[np.ones((X.shape[0], 1)), X]
-        self.y = y
+        X = np.c_[np.ones((X.shape[0], 1)), X]
 
-        A = np.eye(self.X.shape[1])
+        A = np.eye(X.shape[1])
         A[0][0] = 0
 
-        self.intercept_, *self.coef_ = np.dot(np.linalg.inv(self.X.T @ self.X + self.alpha * A) @ self.X.T, self.y)
+        self.intercept_, *self.coef_ = np.dot(np.linalg.inv(X.T @ X + self.alpha * A) @ X.T, y)
 
     def predict(self, X):
         X = np.c_[np.ones((X.shape[0], 1)), X]
+        w = [self.intercept_, *self.coef_]
 
-        return np.dot(X, [self.intercept_, *self.coef_])
+        return np.dot(X, w)
