da7b9b795234c5349f433ab0a1f1f5d4abf33fe7
Vipul Gharde <vipul.gharde@gmail.com>
2022-05-02 02:19:26 -0400

    Rearrange imports

--- learnml/linear_model/__init__.py
+++ learnml/linear_model/__init__.py
@@ -1,7 +1,7 @@
 from ._base import LinearRegression
-from ._stochastic_gradient import SGDClassifier, SGDRegressor
 from ._ridge import Ridge
 from ._perceptron import Perceptron
+from ._stochastic_gradient import SGDClassifier, SGDRegressor
 
 __all__ = ['LinearRegression',
            'Perceptron',
