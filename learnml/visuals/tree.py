d8c447b948373a439962d42df9fc01a86ab1aa17
Vipul Gharde <vipul.gharde@gmail.com>
2022-05-14 10:22:10 -0400

    Update code

--- learnml/visuals/tree.py
+++ learnml/visuals/tree.py
@@ -2,19 +2,21 @@ from graphviz import Digraph
 
 
 def draw_tree(tree):
-    t = Digraph()
-
-    for root in tree.keys():
+    def draw_tree_helper(subtree, t):
+        root = list(subtree.keys())[0]
         t.node(root, root)
-        for edge in tree[root]:
-            child = tree[root][edge]
+
+        for edge in subtree[root]:
+            child = subtree[root][edge]
 
             if type(child) == dict:
-                for ch in child.keys():
-                    t.edge(root, ch, label=edge)
-                    draw_tree(child)
-            else:
-                t.node(root + child, child, shape='box')
-                t.edge(root, root + child, label=str(edge))
+                t.edge(root, list(child.keys())[0], label=edge)
+                draw_tree_helper(child, t)
 
+            else:
+                t.node(edge + child, child, shape='box')
+                t.edge(root, edge + child, label=edge)
+
+    t = Digraph()
+    draw_tree_helper(tree, t)
     t.render(view=True)
