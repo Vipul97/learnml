13bd4c1328c160bc9d2134c2e854c0cb4c3665b6
Vipul Gharde <vipul.gharde@gmail.com>
2024-10-25 02:21:04 -0400

    Update minimum required Python version

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ LearnML is a Python module for Machine Learning and Deep Learning.
 # Installation
 
 ## Requirements
-* Python 3.7+
+* Python 3.10+
 
 ## Stable release
 
--- setup.py
+++ setup.py
@@ -23,14 +23,11 @@ setuptools.setup(
         'Operating System :: POSIX',
         'Operating System :: Unix',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.12',
         'Topic :: Scientific/Engineering',
         'Topic :: Software Development'
     ],
-    python_requires='>=3.7'
+    python_requires='>=3.10'
 )
